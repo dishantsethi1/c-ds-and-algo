4aca628ddd3123aa8c8a0540847d7b81b86894f0
Dishant Sethi <dishantsethi266@gmail.com>
2020-06-03 12:42:55 +0530

    circular linked list

--- circularll.cpp
+++ circularll.cpp
@@ -37,6 +37,11 @@ void printlist(node*head)
   }
 
 }
+
+
+
+
+
 // void splitlist(node*head,node**h1,node**h2)
 // {
 //   node*s=head;
@@ -58,29 +63,97 @@ void printlist(node*head)
 //   f->next=s->next;
 //   s->next=head;
 // }
-bool iscircular(node*head)
-{
-  node*temp=head->next;
-  if(head==NULL) return true ;
-  while(temp!=NULL && temp!=head)
-  {
-    temp=temp->next;
-  }
-  return (temp==head);
 
-}
+
+
+
+
+// bool iscircular(node*head)
+// {
+//   node*temp=head->next;
+//   if(head==NULL) return true ;
+//   while(temp!=NULL && temp!=head)
+//   {
+//     temp=temp->next;
+//   }
+//   return (temp==head);
+//
+// }
+
+
+
+
+
+
+
+// void deletenode(node**head,int key)
+// {
+//   if(*head==NULL) return;
+//   if((((*head)->data)==key) && (((*head)->next)==*head))
+//   {
+//     delete *head;
+//     *head==NULL;
+//   }
+//   node*last=*head,*d;
+//   if(((*head)->data)==key)
+//   {
+//     while(last->next!=(*head))
+//     {
+//       last=last->next;
+//     }
+//     last->next=((*head)->next);
+//     delete *head;
+//     *head=last->next;
+//   }
+//   while(last->next!=(*head) && last->next->data!=key)
+//   {
+//     last=last->next;
+//   }
+//   if(last->next->data==key)
+//   {
+//     d=last->next;
+//     last->next=d->next;
+//     delete d;
+//   }
+//   else
+//   {
+//     cout<<"no such element found";
+//   }
+// }
+
+
+
+
+
+// int countnode(node*head)
+// {int c=0;
+//   node*temp=head;
+//   if(head!=NULL)
+//   {
+//     do {
+//     temp=temp->next;
+//     c++;
+//     } while(temp!=head);
+//   }
+//   return c;
+// }
+
+
+
+
 
 int main()
 {
   node* head=NULL;
   push(&head, 12);
     push(&head, 56);
-    push(&head, 2);
+    push(&head, 5);
     push(&head, 11);
 
 
+
     printlist(head);
-    cout<<iscircular(head);
+
 
       return 0;
 }
